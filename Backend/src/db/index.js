923eb051b8c4fefff64b8a2dd8ce60280bd4162b
Om Sinkar <omsinkar03bit@gmail.com>
2024-02-26 13:45:48 +0530

    Bacckend updated, added microsoft auth

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+// import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\nMongoDB connected to ${DB_NAME} database! DB HOST: ${connectionInstance.connection.host}`);
+        console.log(typeof(process.env.MONGODB_URI))
+        console.log(process.env.PORT)
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`\nMongoDB connected to CodeNEX Atlas database! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB error",error);
         process.exit(1);
---
+++ Backend/src/middlewares/auth.microsoft.js
@@ -0,0 +1,24 @@
+const MicrosoftStrategy = require('passport-microsoft').Strategy;
+const passport = require('passport');
+var MICROSOFT_GRAPH_CLIENT_ID = 'afa6a1bc-c7c9-4e47-843c-3d09d082d268';
+var MICROSOFT_GRAPH_CLIENT_SECRET = '3It8Q~7tVDegc.LIWhKEZAgaobSmCEp_OOnQGdc3';
+const User = require("../models/user");
+
+passport.use(new MicrosoftStrategy({
+  clientID: MICROSOFT_GRAPH_CLIENT_ID,
+  clientSecret: MICROSOFT_GRAPH_CLIENT_SECRET,
+  callbackURL: 'https://intellicampus.in/auth/microsoft/redirect',
+  scope: ['user.read']
+},
+function (accessToken, refreshToken, profile, done) {
+  process.nextTick(function () {  
+    User.findOne({email: profile.emails[0].value }).exec(function(err, userdata){
+      return done(null, userdata);
+
+    });
+  });
+}
+));
+
+
+module.exports = microsoftpassport;
